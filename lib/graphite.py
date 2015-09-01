53f3a7f2ed493604962d4e39ad2d9eea10c6b5df
Michael Rüttgers <mr@westwerk.ac>
2015-09-01 11:47:00 +0200

    Added graphite support for storage of statistical data
    Changed default metrics
    
    Added some notes regarding graphite support to README.md
    
    Code style
    
    Replace spaces in metric names with underscores
    
    Code style
    
    Minor fixes, exception handling, use string formatting
    
    Do not ignore 0 values
    
    If ist not neccessary here, spaces in node id are escaped now
    
    Removed another uneccessary condition
    
    Backported to master

---
+++ lib/graphite.py
@@ -0,0 +1,46 @@
+import socket
+import time
+
+
+class Graphite(object):
+
+    def __init__(self, hostname, port):
+        self.hostname = hostname
+        self.port = int(port)
+
+    def flatten_dict(self, d):
+        def expand(key, value):
+            if isinstance(value, dict):
+                return [('{}.{}'.format(key, k), v) for k, v in self.flatten_dict(value).items()]
+            else:
+                return [(key, value)]
+        items = [item for k, v in d.items() for item in expand(k, v)]
+        return dict(items)
+
+    def update(self, prefix, metrics, nodes):
+        ts = int(time.time())
+
+        sock = socket.socket()
+        sock.connect((self.hostname, self.port))
+
+        for node_id, node in nodes.items():
+            try:
+                if node['flags']['online']:
+                    stats = self.flatten_dict(node['statistics'])
+                    for metric in metrics.split(','):
+                        try:
+                            msg = '{}{}.{} {} {}\n'.format(
+                                prefix,
+                                node_id.replace(' ', '_'),
+                                metric.replace(' ', '_'),
+                                stats[metric], ts
+                            )
+                            sock.send(msg.encode('utf-8'))
+
+                        except KeyError:
+                            pass
+
+            except KeyError:
+                pass
+
+        sock.close()
